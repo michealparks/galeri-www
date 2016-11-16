756e9f1dd48b7ff20b98a11ec001e9291f00e3fb
Micheal Parks <michealparks1989@gmail.com>
2016-11-15 19:32:04 -0500

    update

--- src/components/versions.js
+++ src/components/versions.js
@@ -1,20 +1,26 @@
 const { requestJSON } = require('./request')
 const url = 'https://api.github.com/repos/michealparks/galeri-www/releases/latest'
 const versionEl = document.getElementById('version')
-const macDownload = document.getElementById('download-mac')
+const downloads = document.getElementsByClassName('downloads')
 
 function getMacDownloadURL (v) {
   return `https://github.com/michealparks/galeri-www/releases/download/${v}/Galeri-${v}.dmg`
 }
 
 function onVersionErr () {
-  console.log(this)
+
 }
 
 function onVersionLoad () {
   versionEl.href = this.response.html_url
   versionEl.textContent = `Version ${this.response.tag_name.slice(1)}`
-  macDownload.href = getMacDownloadURL(this.response.tag_name)
+
+  const macURL = getMacDownloadURL(this.response.tag_name)
+
+  for (let i = 0, d, l = downloads.length; i < l; ++i) {
+    d = downloads[i]
+    d.querySelector('#download-mac').href = macURL
+  }
 }
 
 function getLatestVersion () {
