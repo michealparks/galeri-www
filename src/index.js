549c8a12509c911d730be67d32465b7b7f5eec16
Micheal <michealparks1989@gmail.com>
2019-10-27 22:56:15 -0400

    add chrome

--- src/index.js
+++ src/index.js
@@ -1,69 +1,48 @@
-/* global Image */
-const storage = require('./components/storage')
-const request = require('./components/request')
-const getLatestVersion = require('./components/versions')
-const setPlatformText = require('./components/platform')
-const shuffle = require('./components/shuffle')
-const storedImages = storage.get('images')
-const wikiErrTimeout = 5
-let wikiErrCount = 0
-
-if (storedImages === null || storedImages.length === 0) {
-  getWikiJSON()
-} else {
-  setBackgroundImage(storedImages, 'background-image', function () {
-    setBackgroundImage(storedImages, 'section-2')
-  })
+const request = (url, onload) => {
+  const req = new window.XMLHttpRequest()
+  req.open('GET', url, true)
+  req.responseType = 'json'
+  req.onload = onload
+  req.send()
 }
 
-getLatestVersion()
-setPlatformText()
-
-function getWikiJSON () {
+const getWikiJSON = () => {
   const wikiAPI = 'https://en.wikipedia.org/w/api.php'
-  const wikiURL = wikiAPI + '?action=parse&prop=text&page=Wikipedia:Featured%20pictures/Artwork/Paintings&format=json&origin=*'
-
-  return request(wikiURL, onWikiError, onWikiLoad)
-}
-
-function onWikiError () {
-  if (++wikiErrCount > wikiErrTimeout) {
-    return getWikiJSON()
-  }
-}
-
-function onWikiLoad () {
-  const template = document.createElement('template')
-  template.innerHTML = this.response.parse.text['*']
-  const imgs = template.content.querySelectorAll('.gallerybox img')
-  const images = []
-  for (let i = imgs.length - 1; i > -1; --i) {
-    images.push('https:' + imgs[i].getAttribute('src'))
-  }
-
-  shuffle(images)
-
-  setBackgroundImage(images, 'background-image', function () {
-    setBackgroundImage(images, 'section-2')
+  const wikiURL = `${wikiAPI}?action=parse&prop=text&page=Wikipedia:Featured%20pictures/Artwork/Paintings&format=json&origin=*`
+  return request(wikiURL, function () {
+    const template = document.createElement('template')
+    template.innerHTML = this.response.parse.text['*']
+    const imgs = template.content.querySelectorAll('.gallerybox img')
+    const r1 = Math.floor(Math.random() * imgs.length)
+    const r2 = Math.floor(Math.random() * imgs.length)
+    const src1 = `https:${imgs[r1].src}`
+    const src2 = `https:${imgs[r2].src}`
+    setBackgroundImage(src1, 'background-image')
+    setBackgroundImage(src2, 'section-2')
   })
 }
 
-function setBackgroundImage (images, target, next) {
-  const random = images.pop()
-  const img = new Image()
-
-  img.onload = function () {
-    document.getElementById(target)
-      .setAttribute('style', 'background-image:url("' + img.src + '")')
-  }
-
-  img.onerror = function () {
-    setBackgroundImage(images, target)
-  }
-
-  img.src = random.replace(/[0-9]{3,4}px/, '2000px')
-
-  storage.set('images', images)
-
-  if (next) next()
+const setBackgroundImage = (src, target) => {
+  document.getElementById(target).style = `background-image:url("${src}")`
 }
+
+const getMacDownloadURL = (v) => {
+  return `https://github.com/michealparks/galeri-www/releases/download/${v}/Galeri-${v}.dmg`
+}
+
+const getLatestVersion = () => {
+  const url = 'https://api.github.com/repos/michealparks/galeri-www/releases/latest'
+  return request(url, function () {
+    const versionEl = document.getElementById('version')
+    versionEl.href = this.response.html_url
+    versionEl.textContent = 'Version ' + this.response.tag_name.slice(1)
+
+    const macURL = getMacDownloadURL(this.response.tag_name)
+    for (const el of document.querySelectorAll('.downloads')) {
+      el.querySelector('#download-mac').href = macURL
+    }
+  })
+}
+
+getWikiJSON()
+getLatestVersion()
